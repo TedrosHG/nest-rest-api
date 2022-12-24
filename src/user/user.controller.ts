e6e013118f1ff78be217501ade44dfad9fa83fc5
TedrosHG <tedrosastu@gmail.com>
2022-12-25 00:01:15 +0300

    user model

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,26 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { User } from '@prisma/client';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
+@UseGuards(JwtGuard)
 @Controller('user')
 export class UserController {
-
-    @UseGuards(AuthGuard('jwt'))
+    constructor(private userService: UserService){}
     @Get('me')
-    getMe(@Req() req: Request) {
-        console.log({user:req.user});
-        
-        return 'user info'
+    getMe(@GetUser() user: User) {
+        return user
+    }
+
+    @Patch('edit')
+    editUser(
+        @GetUser('id') userId: number,
+        @Body()dto: EditUserDto
+    ) {
+return this.userService.editUser(userId, dto)
     }
 }
 
