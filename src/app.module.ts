17dc169c6df1566b6390794d6d9dc7aa1959fbc3
TedrosHG <tedrosastu@gmail.com>
2022-12-24 15:27:40 +0300

    configuring docker and prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
